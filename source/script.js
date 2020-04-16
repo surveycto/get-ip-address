02249913370df50236a061789729ecacbb46c7bf
Max S. Haberman <max@maxshaberman.com>
2020-04-15 20:06:57 -0400

    Moved functions and removed unnecessary vars, re-enabled goToNextField()

--- source/script.js
+++ source/script.js
@@ -1,25 +1,41 @@
-// Detect platform
-var isWebCollect = (document.body.className.indexOf("web-collect") >= 0);
-var isAndroid = (document.body.className.indexOf("android-collect") >= 0);
-var isIOS = (document.body.className.indexOf("ios-collect") >= 0);
-var hash_parameter = getPluginParameter("hash");
-var ip_address ="";
-
 //Basic hash function to convert to 32bit integer
 function stringToHash(string) {
 
-    var hash = 0;
+  var hash = 0;
 
-    if (string.length == 0) return hash;
+  if (string.length == 0) return hash;
 
-    for (i = 0; i < string.length; i++) {
-        char = string.charCodeAt(i);
-        hash = ((hash << 5) - hash) + char;
-        hash = hash & hash;
-    }
+  for (i = 0; i < string.length; i++) {
+      char = string.charCodeAt(i);
+      hash = ((hash << 5) - hash) + char;
+      hash = hash & hash;
+  }
 
-    return hash;
-};
+  return hash;
+}
+
+// If the field label or hint contain any HTML that isn't in the form definition, then the < and > characters will have been replaced by their HTML character entities, and the HTML won't render. We need to turn those HTML entities back to actual < anmd > characters so that the HTML renders properly. This will allow you to render HTML from field references in your field label or hint.
+function unEntity(str){
+  return str.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+}
+
+// Define what happens when the user attempts to clear the response
+function clearAnswer() {
+  setAnswer();
+}
+
+////End global functions
+
+
+var hash_parameter = getPluginParameter("hash");
+var ip_address ="";
+
+if (fieldProperties.LABEL) {
+  document.querySelector(".label").innerHTML = unEntity(fieldProperties.LABEL);
+}
+if (fieldProperties.HINT) {
+  document.querySelector(".hint").innerHTML = unEntity(fieldProperties.HINT);
+}
 
 //Once ip field is captured, set the answer
 var finishedJsonLoad = function() {
@@ -27,7 +43,7 @@ var finishedJsonLoad = function() {
   if(hash_parameter=="yes") {
     //scramble the collected IP address
     var hashed_ip = stringToHash(ip_address);
-    //set answer to collected ip Address
+    //set answer to hashed collected ip Address
     setAnswer(hashed_ip);
   }
   else {
@@ -35,23 +51,8 @@ var finishedJsonLoad = function() {
     setAnswer(ip_address);
   }
   //go to next field once ip address is set
-  //goToNextField();
+  goToNextField();
 };
-
-
-// Find the input element
-var input = document.getElementById('text-field');
-
-// If the field label or hint contain any HTML that isn't in the form definition, then the < and > characters will have been replaced by their HTML character entities, and the HTML won't render. We need to turn those HTML entities back to actual < anmd > characters so that the HTML renders properly. This will allow you to render HTML from field references in your field label or hint.
-function unEntity(str){
-    return str.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
-}
-if (fieldProperties.LABEL) {
-    document.querySelector(".label").innerHTML = unEntity(fieldProperties.LABEL);
-}
-if (fieldProperties.HINT) {
-    document.querySelector(".hint").innerHTML = unEntity(fieldProperties.HINT);
-}
 
 //use JQuery to get IP address ipify.org
 $.get("https://api.ipify.org", function(e) {
@@ -59,8 +60,3 @@ $.get("https://api.ipify.org", function(e) {
   $('#ip_value').text(ip_address);//display collected value to screen
   finishedJsonLoad();// callback to access ip_address globally
 });
-
-// Define what happens when the user attempts to clear the response
-function clearAnswer() {
-    input.value = '';
-}
